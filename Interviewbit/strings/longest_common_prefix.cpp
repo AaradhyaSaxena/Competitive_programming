cd6b4f11fab74186b661babaec7ab2cddfc9564b
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-03-25 21:06:39 +0530

    string

---
+++ Interviewbit/strings/longest_common_prefix.cpp
@@ -0,0 +1,59 @@
+https://www.interviewbit.com/problems/longest-common-prefix/
+
+// string Solution::longestCommonPrefix(vector<string> &A) {
+//     auto num = A.size();
+//     int minimum = INT_MAX;
+//     cout<<num<<endl;
+    
+//     for(int i=0; i<num; i++){
+//         auto s = A[i].length();
+//         if (minimum > s)
+//             minimum = s;
+//     }
+//     cout<<minimum<<endl;
+//     int cnt =0;
+//     int j=0;
+//     while(j<=minimum)
+//     {
+//     for(auto i=0; i<num-1; i++)
+//     {
+//         // cout<<"lol"<<endl;
+//         if(A[i][j]!=A[i+1][j]){
+//             cout<<"lol"<<endl;
+//             goto end;
+            
+//         }
+//     }
+//     cnt++;
+//     j++;
+//     }
+//     end: string ans;
+//     // cout<<"lol"<<endl;
+//     for(int i=0; i<cnt; i++){
+//         ans.append(A[i]);
+//     }
+//     return ans;
+// }
+
+string Solution::longestCommonPrefix(vector<string> &A) {
+    string result = "";
+    auto n = A.size();
+    int j = INT_MAX;  ///// smallest length of string in vector A
+    for (auto i = 0; i<n; ++i){
+        auto s = A[i].length();
+        if (j > s)
+            j = s;
+    }
+    for (auto i = 0; i<j; ++i){
+        auto temp = A[0][i];
+        int k = 1;
+        while (k<n){
+            if (temp==A[k][i])
+                ++k;
+            else
+                return result;
+        }
+        result += temp; 
+    }
+    return result;
+}
---
+++ Interviewbit/strings/palindrome_string.cpp
@@ -0,0 +1,52 @@
+https://www.interviewbit.com/problems/palindrome-string/
+
+// bool isAlphabet(char c){
+//     if((c>='0'&&c<='9')||(c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+//         return true;
+//     return false;
+// }
+// int Solution::isPalindrome(string A) {
+//     int N = A.length();
+//     int i = 0, j = N-1;
+//     int left = i, right= j;
+    
+//     int cnt=0;
+//     for(int i=0; i<N; i++){
+//         if(isAlphabet(A[i])){
+//             cnt++;
+//         }
+//     }
+//     if(cnt==0){
+//         return 1;
+//     }
+    
+//     while(i<=j){
+//         while(!isAlphabet(A[i])){
+//             i++;
+//             // cout<<i<<endl;
+//         }   
+//         while(!isAlphabet(A[j])){
+//             j--;
+//             // cout<<i<<endl;
+//         }
+//         left = A[i]; right = A[j];
+//         i++; j--;
+//         if((left != right) && abs(left-right)!=32){
+//             // cout<<"lol"<<endl;
+//             return 0;
+//         }
+//     }
+//     return 1;
+// }
+///////////////////////////////////////////////
+bool isPalindrome(string s) {
+    int i = 0, j = (int)s.size() - 1;
+    while(i < j){
+        while(i < j && !isalnum(s[i])) i++;
+        while(i < j && !isalnum(s[j])) j--;
+        if (toupper(s[i]) != toupper(s[j])) return false; 
+        i++;
+        j--;
+    }
+    return true;
+}
