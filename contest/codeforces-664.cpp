8949be6e8ccf1e5ade6cc2d6308e05b1235760de
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-08-13 02:23:54 +0530

    added_664

--- contest/codeforces-664.cpp
+++ contest/codeforces-664.cpp
@@ -124,114 +124,114 @@ int main(){
 ////////////////////////////
 ////////  p2  //////////////
      
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// bool visited[100][100];
-// int movex[4] = {0,1,0,-1};
-// int movey[4] = {1,0,-1,0};
-// vector<pair<int,int>> ans;
+bool visited[100][100];
+int movex[4] = {0,1,0,-1};
+int movey[4] = {1,0,-1,0};
+vector<pair<int,int>> ans;
 
-// bool islegal(int n, int m, int x, int y){
-// 	if(x>0 and x<=n and y>0 and y<=m){
-// 		return true;
-// 	}return false;
-// }
+bool islegal(int n, int m, int x, int y){
+	if(x>0 and x<=n and y>0 and y<=m){
+		return true;
+	}return false;
+}
 
-// void move(int n,int m,int &x,int &y, int &left){
-// 	if(left == 0)
-// 		return;
-// 	for(int k=1; k<max(n,m); k++){
-// 		for(int i=0; i<4; i++){
-// 			int X = x + k*movex[i], Y = y + k*movey[i];
-// 			if(islegal(n,m,X,Y) and visited[X-1][Y-1]==false){
-// 				x = X;
-// 				y = Y;
-// 				visited[x-1][y-1] = true;
-// 				ans.push_back({x,y});
-// 				left--;
-// 				move(n,m,x,y,left);
-// 			}
-// 		}
-// 	}
-// 	if(left !=0){
-// 		ans.pop_back();
-// 	}
-// }
+void move(int n,int m,int &x,int &y, int &left){
+	if(left == 0)
+		return;
+	for(int k=1; k<max(n,m); k++){
+		for(int i=0; i<4; i++){
+			int X = x + k*movex[i], Y = y + k*movey[i];
+			if(islegal(n,m,X,Y) and visited[X-1][Y-1]==false){
+				x = X;
+				y = Y;
+				visited[x-1][y-1] = true;
+				ans.push_back({x,y});
+				left--;
+				move(n,m,x,y,left);
+			}
+		}
+	}
+	if(left !=0){
+		ans.pop_back();
+	}
+}
 
-// int main(){
-// 	int n,m,x,y,t;
-// 	cin>>n>>m>>x>>y;
-// 	t = n*m;
-// 	memset(visited, false, sizeof(visited));
-// 	visited[x-1][y-1] = true;
-// 	cout<<x<<" "<<y<<endl;
-// 	int left = n*m -1;
-// 	move(n,m,x,y,left);
+int main(){
+	int n,m,x,y,t;
+	cin>>n>>m>>x>>y;
+	t = n*m;
+	memset(visited, false, sizeof(visited));
+	visited[x-1][y-1] = true;
+	cout<<x<<" "<<y<<endl;
+	int left = n*m -1;
+	move(n,m,x,y,left);
 
-// 	if(left==0){
-// 		for(int i=0; i<ans.size(); i++){
-// 			cout<<ans[i].first<<" "<<ans[i].second<<endl;
-// 		}
-// 	}
-// }
+	if(left==0){
+		for(int i=0; i<ans.size(); i++){
+			cout<<ans[i].first<<" "<<ans[i].second<<endl;
+		}
+	}
+}
 
 
 ///////////////////////////////
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// bool checkP(int r, int g, int b, int w){
-// 	int nodd = 0, neven = 0;
-// 	if(r%2==0){
-// 		neven++;
-// 	}else{
-// 		nodd++;
-// 	}
-// 	if(g%2==0){
-// 		neven++;
-// 	}else{
-// 		nodd++;
-// 	}
-// 	if(b%2==0){
-// 		neven++;
-// 	}else{
-// 		nodd++;
-// 	}
-// 	if(w%2==0){
-// 		neven++;
-// 	}else{
-// 		nodd++;
-// 	}
-// 	if(nodd==1 and neven==3){
-// 		return true;
-// 	}
-// 	if(nodd==0){
-// 		return true;
-// 	} 
-// 	return false;
-// }
+bool checkP(int r, int g, int b, int w){
+	int nodd = 0, neven = 0;
+	if(r%2==0){
+		neven++;
+	}else{
+		nodd++;
+	}
+	if(g%2==0){
+		neven++;
+	}else{
+		nodd++;
+	}
+	if(b%2==0){
+		neven++;
+	}else{
+		nodd++;
+	}
+	if(w%2==0){
+		neven++;
+	}else{
+		nodd++;
+	}
+	if(nodd==1 and neven==3){
+		return true;
+	}
+	if(nodd==0){
+		return true;
+	} 
+	return false;
+}
 
-// int main(){
-// 	int t; 
-// 	cin>>t;
-// 	while(t--){
-// 		long long int r,g,b,w;
-// 		cin>>r>>g>>b>>w;
-// 		if(checkP(r,g,b,w)){
-// 			cout<<"YES"<<endl;
-// 		}else{
-// 			if(r>0 and g>0 and b>0){
-// 				if(checkP(r-1,g-1,b-1,w+3)){
-// 					cout<<"YES"<<endl;
-// 				}else{
-// 					cout<<"NO"<<endl;
-// 				}
-// 			}else{
-// 				cout<<"NO"<<endl;
-// 			}
-// 		}
-// 	}
-// }
+int main(){
+	int t; 
+	cin>>t;
+	while(t--){
+		long long int r,g,b,w;
+		cin>>r>>g>>b>>w;
+		if(checkP(r,g,b,w)){
+			cout<<"YES"<<endl;
+		}else{
+			if(r>0 and g>0 and b>0){
+				if(checkP(r-1,g-1,b-1,w+3)){
+					cout<<"YES"<<endl;
+				}else{
+					cout<<"NO"<<endl;
+				}
+			}else{
+				cout<<"NO"<<endl;
+			}
+		}
+	}
+}
 
 
