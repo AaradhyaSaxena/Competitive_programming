609c50ab9e5010efe5f10a139983914ec4b0f04b
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-09-13 23:51:37 +0530

    cf-gfg

--- contest/codeforces-670.cpp
+++ contest/codeforces-670.cpp
@@ -85,7 +85,7 @@ int main(){
 
 //////////////
 
-////// p3
+////// p3 /// incorrect
 
 #include<bits/stdc++.h> 
 #define endl '\n'
@@ -151,36 +151,102 @@ int main(){
     return 0;
 }
 
+///// p3 editorial
 
-///////////
-
-#include<bits/stdc++.h> 
-#define endl '\n'
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
 using namespace std;
-typedef long long int ll;
 
-int helper(ll B[], ll C[]){
-
+int n, size[100005], fa[100005], minn=1e9, cent1, cent2;
+vector<int> g[100005];
+void dfs(int x,int f){
+    fa[x]=f,size[x]=1;
+    int mx=0;
+    for(int y:g[x]){
+        if(y==f)continue;
+        dfs(y,x);
+        size[x]+=size[y];
+        mx=max(mx,size[y]);
+    }
+    mx=max(mx,n-size[x]);
+    if(mx<minn)minn=mx,cent1=x,cent2=0;
+    else if(mx==minn)cent2=x;
 }
-     
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t;
-    cin >> t;
-    while(t--){
-    	ll n;
-    	ll arr[n];
-    	ll B[n]; ll C[n] = {0};
-    	for(ll i=0; i<n; i++){
-    		cin>>arr[i];
-    		B[i] = arr[i];
-    	}
-    	
 
+int S;
+
+void dfs2(int x,int f){
+    if(g[x].size()==1){
+        S=x;
+        return ;
+    }
+    for(int y:g[x]){
+        if(y==f)continue;
+        dfs2(y,x);
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n,cent1=cent2=0,minn=1e9;
+        for(int i=1;i<=n;i++) g[i].clear(),fa[i]=0;
+        for(int i=1;i<n;i++){
+            int x,y;
+            cin>>x>>y;
+            g[x].push_back(y), g[y].push_back(x);
+        }
+        dfs(1,0);
+        if(!cent2){
+            printf("1 %d\n1 %d\n",g[1][0],g[1][0]);
+            continue;
+        }
+        if(fa[cent1] != cent2) swap(cent1, cent2);
+        dfs2(cent1, cent2);
+        printf("%d %d\n%d %d\n",S,fa[S],S,cent2);
     }
     return 0;
+}
+
+/////////// p4
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; 
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int sumg = 0, suml = 0;
+    for(int i=1; i<n; i++){
+        if(a[i]>a[i-1]) sumg += a[i] - a[i-1];
+        else suml += a[i-1] - a[i];
+    }
+    long long int ans = ceil((double)(a[0]+sumg)/2.0);
+    cout<<ans<<endl;
+
+    int q; cin>>q;
+    while(q--){
+        int l, r, x, a1 = a[0];
+        cin>>l>>r>>x;
+
+        for(int i=l-1; i<r; i++){
+            a[i] += x;
+        }
+        sumg = 0, suml = 0;
+        for(int i=1; i<n; i++){
+            if(a[i]>a[i-1]) sumg += a[i] - a[i-1];
+            else suml += a[i-1] - a[i];
+        }
+        ans = ceil((double)(a[0]+sumg)/2.0);
+        cout<<ans<<endl;
+    }
 }
 
 ///////////
