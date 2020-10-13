6ca38b73034e2eda0f20dbd00969e9c3d6906532
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-10-13 06:22:39 +0530

    p3 binary-listing

--- contest/tree-cf.cpp
+++ contest/tree-cf.cpp
@@ -135,6 +135,93 @@ int main(){
 	}
 }
 
+//////////////// p3
 
-////////////////
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+
+int n, l;
+int timer;
+vector<int> tin, tout;
+vector<vector<int>> up;
+vector<int> depth;
+
+void dfs(int v, int p, vector<vector<int>> &adj)
+{
+    tin[v] = ++timer;
+    up[v][0] = p;
+    for (int i = 1; i <= l; ++i){
+    	up[v][i] = up[up[v][i-1]][i-1];	
+    }
+    for(int to : adj[v]) {
+    	if(to != p){
+    		depth[to] = depth[v] + 1;
+    		dfs(to, v, adj);
+        }
+    }
+    tout[v] = ++timer;
+}
+
+bool is_ancestor(int u, int v){
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+
+int lca(int u, int v){
+
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
+    for (int i = l; i >= 0; --i) {
+        if (!is_ancestor(up[u][i], v))
+            u = up[u][i];
+    }
+    return up[u][0];
+}
+
+void preprocess(int root, vector<vector<int>> adj) {
+    tin.resize(n+1);
+    tout.resize(n+1);
+    timer = 0;
+    l = ceil(log2(n+1));
+    up.assign(n+1, vector<int>(l+1));
+    dfs(root, root, adj);
+}
+
+int main(){
+	cin>>n;
+	vector<vector<int>> adj(n+1);
+	for(int i=0; i<n-1; i++){
+		int x,y;
+		cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+	depth.resize(n+2);
+	for(int i=0; i<n+1; i++) depth[i] = 0;
+	preprocess(1,adj);
+
+	int q;
+	cin>>q;
+	for(int i=0; i<q; i++){
+		int x,y,z;
+		cin>>x>>y>>z;
+		int lc = lca(x,y);
+		int distxy = depth[x] + depth[y] - 2*depth[lc];
+		if(distxy<=z){
+			cout<<y<<endl;
+		}
+		else if(depth[x]-depth[lc]>=z){
+			cout<<up[x][z-1]<<endl;
+		}
+		else{
+			int left = z - (depth[x]-depth[lc]);
+			int dy = (depth[y]- depth[lc]) - left;
+			cout<<up[y][dy]<<endl;
+		}
+	}
+}
+
 
