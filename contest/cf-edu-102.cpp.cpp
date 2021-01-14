20812f0de3dffe2c5542b660981b81a9469eb2d1
AaradhyaSaxena <aaradhya271197@gmail.com>
2021-01-15 04:49:44 +0530

    cf-edu-102

--- contest/cf-edu-102.cpp.cpp
+++ contest/cf-edu-102.cpp.cpp
@@ -1,153 +1,74 @@
 /////////////////// A
 
-// #include<bits/stdc++.h> 
-// #define endl '\n'
-// using namespace std;
-// typedef long long int ll;
+#include<bits/stdc++.h> 
+#define endl '\n'
+using namespace std;
+typedef long long int ll;
      
-// int main(){
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     int t;
-//     cin >> t;
-//     while(t--){
-//     	int n, d; cin>>n>>d;
-//     	int arr[n];
-//     	for(int i=0; i<n; i++){
-//     		cin>>arr[i];
-//     	}
-//     	sort(arr,arr+n);
-//         if(arr[n-1]<=d){
-//             cout<<"YES"<<endl;
-//         }
-//         else if(arr[0]+arr[1]<=d){
-//             cout<<"YES"<<endl;
-//         }
-//         else{
-//             cout<<"NO"<<endl;
-//         }
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+    	int n, d; cin>>n>>d;
+    	int arr[n];
+    	for(int i=0; i<n; i++){
+    		cin>>arr[i];
+    	}
+    	sort(arr,arr+n);
+        if(arr[n-1]<=d){
+            cout<<"YES"<<endl;
+        }
+        else if(arr[0]+arr[1]<=d){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
     	
-//     }
-//     return 0;
-// }
+    }
+    return 0;
+}
 
 /////////////////// B
-
-// #include<bits/stdc++.h> 
-// #define endl '\n'
-// using namespace std;
-// typedef long long int ll;
-
-// int gcd(int m, int n) {
-//     if(m < n) swap(m, n);
-//     if(n == 0) return m;
-//    return gcd(m % n, n);
-// }
-
-// int lcm(int a, int b){
-//     return (a*b)/gcd(a,b);
-// }
-
-// string helper(string a, int n){
-//     string ans = "";
-//     for(int i=0; i<n; i++){
-//         ans += a;
-//     }
-//     return ans;
-// }
-
-// int isDiv(string a, string b){
-//     int n = a.length(), m = b.length();
-//     if(n%m==0){
-//         for(int i=0; i<m; i++){
-//             if(a[i]!=b[i]){
-//                 return 0;
-//             }
-//         }
-//         return 1;
-//     }
-//     else{
-//         string nn = helper(a, m);
-//         for(int i=0; i<n*m; i++){
-//             if(nn[i]!=b[i%m]){
-//                 return 0;
-//             }
-//         }
-//         return 2;
-//     }
-// }
-     
-// int main(){
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     int t;
-//     cin >> t;
-//     while(t--){
-//         string a, b;
-//         cin>>a>>b;
-//         int n = a.length(), m = b.length();
-//         int gg = lcm(n,m), flag = 0;
-//         for(int i=0; i<max(n,m); i++){
-//             if(a[i%n]!=b[i%m]){
-//                 flag = 1;
-//                 cout<<-1<<endl;
-//             }
-//         }
-//         if(flag==0){
-//             if(n>m){
-//                 if(isDiv(a,b)==2) cout<<helper(a,gg/n)<<endl;
-//                 else if(isDiv(a,b)==1) cout<<a<<endl;
-//                 else cout<<-1<<endl;
-//             }
-//             else{
-//                 if(isDiv(b,a)==2) cout<<helper(b,gg/m)<<endl;
-//                 else if(isDiv(b,a)==1) cout<<b<<endl;
-//                 else cout<<-1<<endl;
-//             }
-//         }
-//     }
-//     return 0;
-// }
-
-// ///////////////// D
 
 #include<bits/stdc++.h> 
 #define endl '\n'
 using namespace std;
 typedef long long int ll;
      
-// int main(){
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(0); cout.tie(0);
-//     int t;
-//     cin >> t;
-//     while(t--){
-//         int n,m; cin>>n>>m;
-//         string str; cin>>str;
-//         int arr[n];
-//         for(int i=0; i<n; i++){
-//             arr[i] = (str[i]=='+')? 1: -1;
-//         }
-//         for(int i=0; i<m; i++){
-//             int l,r;
-//             cin>>l>>r;
-//             int minn = 0, maxx = 0, x=0;
-//             for(int i=1; i<l; i++){
-//                 x += arr[i-1];
-//                 maxx = max(maxx, x); minn = min(minn, x);
-//             }
-//             for(int i=r+1; i<=n; i++){
-//                 x += arr[i-1];
-//                 maxx = max(maxx, x); minn = min(minn, x);
-//             }
-//             cout<<maxx-minn+1<<endl;
-//         }
-//     }
-//     return 0;
-// }
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        string a,b;
+        cin>>a>>b;
+        int x = a.length(), y = b.length();
+        int z = x*y/(__gcd(x,y));
+        string c; int flag =0;
+        for(int i=0;i<z;i++){
+            if(a[i%x]!=b[i%y]) flag =1;
+            else c += a[i%x];
+        }
+        if(flag==0) cout<<c<<endl;
+        else cout<<-1<<endl;
+    }
+    return 0;
+}
 
+//////////////// D
+
+//// incorrect
+#include<bits/stdc++.h> 
+#define endl '\n'
+using namespace std;
+typedef long long int ll;
+     
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -188,17 +109,68 @@ int main(){
             int l,r;
             cin>>l>>r;
             int minn = 0, maxx = 0, x=0;
-            int dmax = dp[n][0]-dp[min(r+1,n)][0], dmin = dp[min(r+1,n)][1]-dp[r+1][1];
+            int dmax = dp[n][0]-dp[min(r+1,n)][0], dmin = dp[n][1]-dp[min(r+1,n)][1];
+            cout<<"dmax = "<<dmax<<"\t"<<"dmin = "<<dmin<<endl;
 
             maxx = dp[l-1][0]; minn = dp[l-1][1];
             cout<<"maxx = "<<maxx<<"\t"<<"minn = "<<minn<<endl;
 
-            x += (dp[l][2] - dp[r][3]);
-            cout<<"x = "<<x<<endl;
-            maxx = max(maxx, x+dmax), minn = min(minn, x+dmin);
+            if(r<n){
+                x += (dp[max(l,0)][2] - dp[min(n,r)][3]);
+                cout<<"x = "<<x<<endl;
+                maxx = max(maxx, x+dmax), minn = min(minn, x+dmin);
+            }
 
             cout<<"maxx = "<<maxx<<"\t"<<"minn = "<<minn<<endl;
             cout<<maxx-minn+1<<endl;
+        }
+    }
+    return 0;
+}
+////////////////////////
+
+#include <stdio.h>
+#define N   200000
+ 
+int min(int a, int b) { return a < b ? a : b; }
+int max(int a, int b) { return a > b ? a : b; }
+ 
+int main() {
+    int t;
+  
+    scanf("%d", &t);
+    while (t--) {
+        static char cc[N + 1];
+        static int pp[N], ppl[N], ppr[N], qql[N], qqr[N];
+        int n, m, i, p, l, r;
+ 
+        scanf("%d%d", &n, &m);
+        scanf("%s", cc);
+        p = 0;
+        for (i = 0; i < n; i++) {
+            p += cc[i] == '+' ? 1 : -1;
+            pp[i] = p;
+            ppl[i] = min(i == 0 ? 0 : ppl[i - 1], p), ppr[i] = max(i == 0 ? 0 : ppr[i - 1], p);
+        }
+        l = 0, r = 0;
+        for (i = n - 1; i >= 0; i--) {
+            l += cc[i] == '+' ? 1 : -1, r += cc[i] == '+' ? 1 : -1;
+            l = min(l, 0), r = max(r, 0);
+            qql[i] = l, qqr[i] = r;
+        }
+        while (m--) {
+            int l, r, p, pl, pr, ql, qr;
+ 
+            scanf("%d%d", &l, &r), l--, r--;
+            if (l == 0)
+                p = pl = pr = 0;
+            else
+                p = pp[l - 1], pl = ppl[l - 1], pr = ppr[l - 1];
+            if (r + 1 == n)
+                ql = qr = 0;
+            else
+                ql = qql[r + 1], qr = qqr[r + 1];
+            printf("%d\n", max(pr, p + qr) - min(pl, p + ql) + 1);
         }
     }
     return 0;
